c0889ccca19980bdfafe4c8412fe988d0156a88b
AlonsoS <85506314+Alonso-S-N@users.noreply.github.com>
2025-08-04 16:09:42 -0300

    Add files via upload

---
+++ src/main/src/main/java/frc/robot/Calc.java
@@ -0,0 +1,140 @@
+package frc.robot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.robozin1;
+    public class Calc {
+        private final robozin1 robozin1;
+        private final Joystick joydeliciu = new Joystick(0);
+    
+        private double B_Speed = 0.5, rapidao, rapidao2, seno, X, Y, magnitude;
+    
+        public Calc(robozin1 robozin1) {
+            this.robozin1 = robozin1;
+        }
+    
+        public void calAnalogico() {
+            Y = -joydeliciu.getRawAxis(1);
+            X = joydeliciu.getRawAxis(0);
+            magnitude = Math.max(-1, Math.min(1, Math.hypot(X, Y)));
+            seno = Y / magnitude;
+
+            
+    
+            if (X > 0 && Y > 0) {
+                rapidao = magnitude * B_Speed;
+                rapidao2 = (2 * seno - 1) * magnitude * B_Speed;
+            } else if (X < 0 && Y > 0) {
+                rapidao = (2 * seno - 1) * magnitude * B_Speed;
+                rapidao2 = magnitude * B_Speed;
+            } else if (X >= 0 && Y < 0) {
+                rapidao = magnitude * B_Speed * -1;
+                rapidao2 = (2 * seno + 1) * magnitude * B_Speed;
+            } else if (X < 0 && Y < 0) {
+                rapidao = (2 * seno + 1) * magnitude * B_Speed;
+                rapidao2 = magnitude * B_Speed * -1;
+            }           
+                if (X >= 0.04 || X < -0.04 || Y >= 0.04 || Y < -0.04) {
+                  robozin1.m_leftDrive.set(ControlMode.PercentOutput, rapidao);
+                  robozin1.m_leftDrive2.set(ControlMode.PercentOutput, rapidao);
+                  robozin1.m_rightDrive.set(ControlMode.PercentOutput, rapidao2);
+                  robozin1.m_rightDrive2.set(ControlMode.PercentOutput, rapidao2);
+                } else {
+                  robozin1.m_leftDrive.set(ControlMode.PercentOutput, 0);
+                  robozin1.m_leftDrive2.set(ControlMode.PercentOutput, 0);
+                  robozin1.m_rightDrive.set(ControlMode.PercentOutput, 0);
+                  robozin1.m_rightDrive2.set(ControlMode.PercentOutput, 0);
+                }
+            
+    
+      }
+        public void calPov() {
+            switch (joydeliciu.getPOV()) {
+            case 0:
+                rapidao = 0.5;
+                rapidao2 = 0.5;
+                break;
+            case 45:
+                rapidao = 0.5;
+                rapidao2 = 0.25;
+                break;
+            case 90:
+                rapidao = 0.5;
+                rapidao2 = -0.5;
+                break;
+            case 135:
+                rapidao = 0.25;
+                rapidao2 = -0.25;
+                break;
+            case 180:
+                rapidao = -0.5;
+                rapidao2 = -0.5;
+                break;
+            case 225:
+                rapidao = -0.25;
+                rapidao2 = -0.25;
+                break;
+            case 270:
+                rapidao = -0.5;
+                rapidao2 = 0.5;
+                break;
+            case 315:
+                rapidao = -0.25;
+                rapidao2 = 0.25;
+                break;
+            }
+
+            robozin1.m_leftDrive.set(ControlMode.PercentOutput, rapidao);
+            robozin1.m_leftDrive2.set(ControlMode.PercentOutput, rapidao);
+            robozin1.m_rightDrive.set(ControlMode.PercentOutput, rapidao2);
+            robozin1.m_rightDrive2.set(ControlMode.PercentOutput, rapidao2);
+
+        }
+        public void calButton() {
+            boolean a = joydeliciu.getRawButton(1);
+            boolean b = joydeliciu.getRawButton(2);
+            boolean x = joydeliciu.getRawButton(3);
+    
+            if (a) B_Speed = 0.25;
+            else if (b) B_Speed = 0.5;
+            else if (x) B_Speed = 1.0;
+    
+            rapidao *= B_Speed;
+            rapidao2 *= B_Speed;
+        }
+       public void RTLT(){
+            double RT = joydeliciu.getRawAxis(3);
+            double LT = joydeliciu.getRawAxis(2);
+    
+            if (RT > 0.04) {
+                rapidao = RT * B_Speed;
+                rapidao2 = RT * B_Speed;
+            } else if (LT > 0.04) {
+                rapidao = -LT * B_Speed;
+                rapidao2 = -LT * B_Speed;
+            } 
+        robozin1.m_leftDrive.set(ControlMode.PercentOutput, rapidao);
+        robozin1.m_leftDrive2.set(ControlMode.PercentOutput, rapidao);
+        robozin1.m_rightDrive.set(ControlMode.PercentOutput, rapidao2);
+        robozin1.m_rightDrive2.set(ControlMode.PercentOutput, rapidao2);
+       }
+
+       public void Smart(){
+          SmartDashboard.putNumber("Left Speed", rapidao);
+            SmartDashboard.putNumber("Right Speed", rapidao2);
+            SmartDashboard.putNumber("Magnitude", magnitude);
+            SmartDashboard.putNumber("Seno", seno);
+            SmartDashboard.putNumber("X", X);
+            SmartDashboard.putNumber("Y", Y);
+            SmartDashboard.putNumber("B_Speed", B_Speed);
+            SmartDashboard.putBoolean("A Button", joydeliciu.getRawButton(1));
+            SmartDashboard.putBoolean("B Button", joydeliciu.getRawButton(2));
+            SmartDashboard.putBoolean("X Button", joydeliciu.getRawButton(3));
+            SmartDashboard.putNumber("POV", joydeliciu.getPOV());
+            SmartDashboard.putNumber("RT", joydeliciu.getRawAxis(3));
+            SmartDashboard.putNumber("LT", joydeliciu.getRawAxis(2));
+       } 
+    }
+
+    
---
+++ src/main/src/main/java/frc/robot/commands/commandin1.java
@@ -0,0 +1,66 @@
+
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Calc;
+import frc.robot.subsystems.robozin1;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+
+public class commandin1 extends  Command {
+
+  private final robozin1 robozin1;
+  protected final Calc calc;
+
+  public commandin1(robozin1 robo, Calc cal) {
+   this.robozin1 = robo;
+   this.calc = cal;
+    addRequirements(robo);
+    CommandScheduler.getInstance().setDefaultCommand(robozin1,
+    new RunCommand(() -> {
+      calc.calAnalogico();
+      calc.calPov();
+      calc.calButton();
+      calc.RTLT();
+    }, robozin1)
+  );
+  }
+
+  @Override
+  public void initialize() {
+    robozin1.m_leftDrive.setNeutralMode(NeutralMode.Brake);
+    robozin1.m_leftDrive2.setNeutralMode(NeutralMode.Brake);
+    robozin1.m_rightDrive.setNeutralMode(NeutralMode.Brake);
+    robozin1.m_rightDrive2.setNeutralMode(NeutralMode.Brake);
+
+    robozin1.m_leftDrive.setInverted(false);
+    robozin1.m_leftDrive2.setInverted(false);
+    robozin1.m_rightDrive.setInverted(true);
+    robozin1.m_rightDrive2.setInverted(true);
+
+    robozin1.m_leftDrive.configNeutralDeadband(0.04);
+    robozin1.m_leftDrive2.configNeutralDeadband(0.04);
+    robozin1.m_rightDrive.configNeutralDeadband(0.04);
+    robozin1.m_rightDrive2.configNeutralDeadband(0.04);
+  }
+
+  @Override
+  public void execute() {
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+
+  public boolean isFinished() {
+      return false;
+  }
+}
