f8d696510a2ec990e6b8f21ff795d9bf503624a5
AlonsoS <85506314+Alonso-S-N@users.noreply.github.com>
2025-08-05 13:52:43 -0300

    Add files via upload

---
+++ src/main/src/main/java/frc/robot/Calcs.java
@@ -0,0 +1,76 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.Constants;
+
+public class Calcs {
+
+   Joystick joyDeliciu;
+
+    public static class DriveSpeeds {
+        public final double left;
+        public final double right;
+        public DriveSpeeds(double left, double right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static DriveSpeeds calculateAnalogDrive(Joystick joyDeliciu, double B_Speed) {
+        double X = joyDeliciu.getX();
+        double Y = -joyDeliciu.getY();
+
+        double magnitude = Math.hypot(X, Y);
+        if (magnitude < Constants.deadZone) return new DriveSpeeds(0, 0);
+
+        magnitude = Math.min(1, Math.max(-1, magnitude));
+        double seno = Y / magnitude;
+
+        double LS = 0, Rs = 0;
+
+        if (X > 0 && Y > 0) {
+            LS = magnitude * B_Speed;
+            Rs = (2 * seno - 1) * magnitude * B_Speed;
+        } else if (X < 0 && Y > 0) {
+            LS = (2 * seno - 1) * magnitude * B_Speed;
+            Rs = magnitude * B_Speed;
+        } else if (X >= 0 && Y < 0) {
+            LS = -magnitude * B_Speed;
+            Rs = (2 * seno + 1) * magnitude * B_Speed;
+        } else if (X < 0 && Y < 0) {
+            LS = (2 * seno + 1) * magnitude * B_Speed;
+            Rs = -magnitude * B_Speed;
+        }
+
+        return new DriveSpeeds(LS, Rs);
+    }
+
+    public static DriveSpeeds calculateTriggerDrive(Joystick joyDeliciu, double B_Speed) {
+        double RT = joyDeliciu.getRawAxis(Constants.RT);
+        double LT = joyDeliciu.getRawAxis(Constants.LT);
+
+        if (RT > Constants.deadZone) {
+            double val = RT * B_Speed;
+            return new DriveSpeeds(val, val);
+        } else if (LT > Constants.deadZone) {
+            double val = -LT * B_Speed;
+            return new DriveSpeeds(val, val);
+        }
+
+        return new DriveSpeeds(0, 0);
+    }
+
+    public static DriveSpeeds calculatePovDrive(Joystick Joydeliciu, double B_Speed) {
+        switch (Joydeliciu.getPOV()) {
+            case 0: return new DriveSpeeds(0.5, 0.5);
+            case 45: return new DriveSpeeds(0.5, 0.25);
+            case 90: return new DriveSpeeds(0.5, -0.5);
+            case 135: return new DriveSpeeds(-0.5, -0.25);
+            case 180: return new DriveSpeeds(-0.5, -0.5);
+            case 225: return new DriveSpeeds(-0.25, -0.5);
+            case 270: return new DriveSpeeds(-0.5, 0.5);
+            case 315: return new DriveSpeeds(-0.25, 0.5);
+            default: return new DriveSpeeds(0, 0);
+        }
+    }
+}
--- src/main/src/main/java/frc/robot/Robot.java
+++ src/main/src/main/java/frc/robot/Robot.java
@@ -1,35 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.robozin1;
-import frc.robot.commands.commandin1;
-import frc.robot.Calc;
+import frc.robot.command.Loc;
 
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
 public class Robot extends TimedRobot {
-  private robozin1 robo;
-  private commandin1 command;
-  private Calc cal;
+  // private Loc locCommand;
+  
+  
+  private RobotContainer m_robotContainer;
+  
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    public Robot() {
+      // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+      // autonomous chooser on the dashboard
+    }
+  
+  
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+}
+  /**
+   * 
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run(); 
+  }
 
-  public Robot() {
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+
   }
-@Override
-  public void robotInit(){
-    robo = new robozin1();
-    cal = new Calc(robo);
-    command = new commandin1(robo,cal);
-  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
 
   }
-   
+
   @Override
-  public void teleopPeriodic() {
-   CommandScheduler.getInstance().run();
-   cal.Smart();
-  } 
+  public void teleopPeriodic() {}
 }
